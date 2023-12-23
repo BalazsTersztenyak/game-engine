6d5ce2d11c38d06885c1f82ceef700a67ef13a24
Balu <Balu@DESKTOP-1D6VN6E>
2023-12-23 20:02:50 +0100

    initial setup

---
+++ SandBox/src/Application.cpp
@@ -0,0 +1,9 @@
+namespace game_engine {
+
+	__declspec(dllimport) void Print();
+
+}
+
+void main() {
+	game_engine::Print();
+}
---
+++ game_engine/src/Test.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace game_engine {
+
+	__declspec(dllexport) void Print();
+
+}
