1de93c82e9b648dbe2699e2854b607fc3943585c
Balu <Balu@DESKTOP-1D6VN6E>
2024-01-05 18:41:18 +0100

    created premake file

---
+++ premake5.lua
@@ -0,0 +1,100 @@
+workspace "Lilac"
+	architecture "x64"
+	startproject "SandBox"
+
+	configurations{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Lilac"
+	location "Lilac"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines{
+			"LL_PLATFORM_WINDOWS",
+			"LL_BUILD_DLL"
+		}
+
+		postbuildcommands{
+			("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/SandBox/\"")
+		}
+
+	filter "configurations:Debug"
+		defines "LL_DEBUG"
+		symbols "On"
+	
+	filter "configurations:Release"
+		defines "LL_RELEASE"
+		optimize "On"
+		
+	filter "configurations:Dist"
+		defines "LL_DEBUG"
+		optimize "On"
+
+
+project "SandBox"
+	location "SandBox"
+	kind "ConsoleApp"
+	language "C++"
+	
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs{
+		"Lilac/vendor/spdlog/include",
+		"Lilac/src"
+	}
+
+	links{
+		"Lilac"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines{
+			"LL_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "LL_DEBUG"
+		symbols "On"
+	
+	filter "configurations:Release"
+		defines "LL_RELEASE"
+		optimize "On"
+		
+	filter "configurations:Dist"
+		defines "LL_DEBUG"
+		optimize "On"
